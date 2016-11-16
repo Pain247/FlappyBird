ea6fb79a9f7a7f7fb8b537aee7250cf77e70844b
Pain247 <dangductungcfc@gmail.com>
2016-11-16 17:23:06 +0700

    initial commit

--- src/bubbleJumping/Bird.java
+++ src/bubbleJumping/Bird.java
@@ -21,22 +21,39 @@ import javax.swing.JPanel;
  */
 public class Bird{
     private static int y=200;
-    private static int x=100;
-  
-    private BufferedImage bubble;
+    private static int x=100;    
+    private int index=0;
+    private BufferedImage bird;
+    private BufferedImage [] list = new BufferedImage[4];
+    private BufferedImage [] list1 = new BufferedImage[4];
     public static int getY(){
         return y;
     }
     public static int getX(){
      return x;
     }
-    public void paint(Graphics g){
+    public BufferedImage [] Img(){
+    for(int i=1;i<5;i++){
         try {
-           bubble= ImageIO.read(new File("C:\\Users\\Dell\\Desktop\\bird\\bird-04.png"));
-            g.drawImage(bubble, x, y,null);
+            list[i-1]=ImageIO.read(new File("C:\\res\\bird-0"+i+".png"));
         } catch (IOException ex) {
             Logger.getLogger(Bird.class.getName()).log(Level.SEVERE, null, ex);
         }
+    
+    }
+    
+    return list;
+    }
+ 
+
+
+   public void paint(Graphics g){
+        list1= Img(); 
+        bird = list1[index];
+        g.drawImage(bird, x, y,null);
+        index++;
+        if(index==3) index=0;
+       
     }
     public void YUp(){
         y=y+1;
@@ -48,5 +65,5 @@ public class Bird{
     x=100;
     y=200;
     }
-    
+
 }
